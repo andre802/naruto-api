0146bd96673445f84a083e3bb49fcd79df42831b
Justin <justinreeves52@gmail.com>
2021-06-11 02:15:16 -0400

    chore

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./lib/routes/server')
 
-app.listen(process.env.port || 3000, () => {
-    console.log(`Listening at http://localhost:${process.env.port || 3000}`);
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Listening at http://localhost:${process.env.PORT || 3000}`);
 })
 
